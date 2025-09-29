efb89b49743410f3f6b74e6871164780362b8eac
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-09-29 07:53:34 +0200

    Migrate to RestClient
    RestClient is not new in Spring Boot 4, but we want to empathize its use
    with RestTemplate going away. It is also superior in test scenario where
    the Mock server can now be shared and managed by the Spring Boot test
    infrastructure.

--- matchmaking/src/main/java/io/spring/sample/matchmaking/lobby/PlayerApiClient.java
+++ matchmaking/src/main/java/io/spring/sample/matchmaking/lobby/PlayerApiClient.java
@@ -1,43 +1,35 @@
 package io.spring.sample.matchmaking.lobby;
 
-import java.util.Collections;
-
-import org.springframework.boot.restclient.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @Component
 class PlayerApiClient {
 
-	final RestTemplate playerProfileClient;
+	private final RestClient playerProfileClient;
 
-	final RestTemplate playerStatsClient;
+	private final RestClient playerStatsClient;
 
-	PlayerApiClient(RestTemplateBuilder clientBuilder, LobbyProperties properties) {
-		this.playerProfileClient = clientBuilder.rootUri(properties.getPlayer().getProfileUrl()).build();
-		this.playerStatsClient = clientBuilder.rootUri(properties.getPlayer().getStatsUrl()).build();
+	PlayerApiClient(RestClient.Builder clientBuilder, LobbyProperties properties) {
+		this.playerProfileClient = clientBuilder.baseUrl(properties.getPlayer().getProfileUrl()).build();
+		this.playerStatsClient = clientBuilder.baseUrl(properties.getPlayer().getStatsUrl()).build();
 	}
 
 	PlayerProfile getPlayerProfile(String playerId) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-		ResponseEntity<PlayerProfile> exchange = playerProfileClient.exchange("/profile/{playerId}", HttpMethod.GET,
-				new HttpEntity<>(headers), PlayerProfile.class, playerId);
-		return exchange.getBody();
-
+		return playerProfileClient.get()
+			.uri("/profile/{playerId}", playerId)
+			.accept(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.body(PlayerProfile.class);
 	}
 
 	PlayerStats getchPlayerStats(String playerId) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-		ResponseEntity<PlayerStats> exchange = playerStatsClient.exchange("/stats/{playerId}", HttpMethod.GET,
-				new HttpEntity<>(headers), PlayerStats.class, playerId);
-		return exchange.getBody();
+		return playerStatsClient.get()
+			.uri("/stats/{playerId}", playerId)
+			.accept(MediaType.APPLICATION_JSON)
+			.retrieve()
+			.body(PlayerStats.class);
 	}
 
 }

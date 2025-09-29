83d64998a514fadd30c1ec0e9fcd2a7677f0b552
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-09-29 08:47:15 +0200

    Enable resilient methods
    Now that we have identified that the stats endpoint is causing trouble,
    we can make the call to the backend resilient by adding @Retryable.
    
    Spring Framework 7 adds core resiliency features, inspired by the Spring
    Retry project that is now superseded by it. @Retryable lets you
    configure which exceptions should lead to a retry, and the retry policy
    to use.

--- matchmaking/src/main/java/io/spring/sample/matchmaking/Application.java
+++ matchmaking/src/main/java/io/spring/sample/matchmaking/Application.java
@@ -2,8 +2,10 @@ package io.spring.sample.matchmaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 public class Application {
 
 	public static void main(String[] args) {
